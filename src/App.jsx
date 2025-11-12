75b7c0b655dffb4e7685ba7f75f0e0d126c56c3e
jeel-patel-04 <itssjeel@gmail.com>
2025-11-12 17:50:43 +0530

    setup shadcn ui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div>Hey Zaalima</div>
+      <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
